551626879587b779e9c9241e5025fe284ec1b11e
ccejudo <cristopher.cm1999@gmail.com>
2021-04-14 14:00:08 -0500

    About carousel addeed

--- frontend/src/layouts/Main/Main.js
+++ frontend/src/layouts/Main/Main.js
@@ -6,10 +6,9 @@ import 'pure-react-carousel/dist/react-carousel.es.css';
 
 //Components
 import { Slide as AnimatedSlide, Fade } from 'react-reveal';
-import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
-import { FaGraduationCap, FaLaptopCode, FaMedal } from 'react-icons/fa';
-
 import { Grid } from '@material-ui/core';
+
+import AboutCarousel from '../../components/AboutCarousel/AboutCarousel';
 
 function Main() {
     return (
@@ -28,64 +27,25 @@ function Main() {
                     </Fade>
                 </div>
             </div>
-            <div id='about' className="section--about">
-                <div className="bg--black container--section">
-                    <div className='title--nice'>
+            <Grid container id='about'>
+                <Grid container item xs={12} className="bg--black container--section">
+                    <Grid item xs={12} className='title--nice'>
                             <h1 className='color--white'><b>ABOUT ME</b></h1>
                             <span></span>
-                    </div>
-                    <div className='about--wrapper'>
-                        <div className='about--text'>
+                    </Grid>
+                    <Grid container item xs={12} className='about--wrapper'>
+                        <Grid item xs={12} md={6} className='about--text'>
                             <p>A third year Computational Engineering student at Tecnológico de Monterrey CSF with an interest in Full-Stack development, Cloud, and Machine Learning. Experienced with different technologies, quick learner and adaptable.<br/><br/>I believe that the skills that I have acquire from different work experiences and competitive university life make me a good leader, and I am always trying to apply and polish them in any opportunity that comes in my way. </p>
                             <div className="carousel--container">
-                                <CarouselProvider
-                                    naturalSlideWidth={60}
-                                    naturalSlideHeight={25}
-                                    totalSlides={3}
-                                >
-                                    <Slider>
-                                        <Slide index={0}>
-                                            <div>
-                                                <h2><FaGraduationCap/>  Education</h2>
-                                                <p>
-                                                    Tecnológico de Monterrey CSF<br/>
-                                                    Bachelor of Computer Engineering<br/>
-                                                    August 2017 - December 2021<br/>
-                                                    GPA: 3.9
-                                                </p>
-                                            </div>
-                                        </Slide>
-                                        <Slide index={1}>
-                                            <div>
-                                                <h2><FaLaptopCode/>  Technologies</h2>
-                                                <ul>
-                                                    <li>ReactJS</li>
-                                                    <li>NodeJS</li>
-                                                    <li>Python</li>
-                                                    <li>CSS</li>
-                                                </ul>
-                                            </div>
-                                        </Slide>
-                                        <Slide index={2}>
-                                            <div>
-                                                <h2><FaMedal/>  Achivements</h2>
-                                                <p>Three Recognitions for Academic Excellence</p>
-                                            </div>
-                                        </Slide>
-                                    </Slider>
-                                    <Grid container justify="space-around">
-                                        <ButtonBack className='carousel--button'>Back</ButtonBack>
-                                        <ButtonNext className='carousel--button'>Next</ButtonNext>
-                                    </Grid>
-                                </CarouselProvider>
+                                <AboutCarousel/>
                             </div>
-                        </div>
-                        <div className='about--image'>
+                        </Grid>
+                        <Grid item xs={12} md={6} className='about--image'>
                             <img src='./images/assets/uke_photo_hex.png' alt='Cris Uke'/>
-                        </div>
-                    </div>
-                </div>
-            </div>
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Grid>
             <div id='portfolio' className="section--portfolio">
                 <div className='bg--white container--section'>
                     <div className='title--nice'>
